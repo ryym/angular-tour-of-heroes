7d7a6b6b7ab8c522cb4c0538a94ddc3d63338794
ryym <ryym.64@gmail.com>
2017-09-09 21:12:31 +0900

    Add dashboard and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,40 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
 import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [
      BrowserModule,
-     FormsModule
+     FormsModule,
+     RouterModule.forRoot([
+       {
+         path: '',
+         redirectTo: '/dashboard',
+         pathMatch: 'full',
+       },
+       {
+         path: 'dashboard',
+         component: DashboardComponent,
+       },
+       {
+         path: 'heroes',
+         component: HeroesComponent,
+       }
+     ])
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent,
   ],
   providers: [
     HeroService
